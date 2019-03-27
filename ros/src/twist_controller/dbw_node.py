68f34bbbd9ac3d3c207511ea9e625cb201cd1984
michaleb <michale.bailey@gmail.com>
2019-03-27 08:42:05 -0700

    updated dbw

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -82,13 +82,13 @@ class DBWNode(object):
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
-            if not None in (self.current_vel, self.linear_vel, self.angular_vel):
+            if self.dbw_enabled and not None in (self.current_vel, self.linear_vel, self.angular_vel):
                 self.throttle, self.brake, self.steering = self.controller.control(self.current_vel,
                                                                  self.angular_vel,
                                                                  self.linear_vel,
                                                                  self.dbw_enabled)
             #                                                     <any other argument you need>)
-            if self.dbw_enabled:
+            #if self.dbw_enabled:
                 self.publish(self.throttle, self.brake, self.steering)
             rate.sleep()
 
