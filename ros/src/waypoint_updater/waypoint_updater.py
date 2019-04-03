b7f8f9bb1cda16fca1ca6e9c0e03bf8482d62691
michaleb <michale.bailey@gmail.com>
2019-04-03 07:06:53 -0400

    updated wp_updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -48,7 +48,7 @@ class WaypointUpdater(object):
 		while not rospy.is_shutdown():
 			if self.pose and self.base_wps:
 				# Get closest waypoint
-				closest_waypoint_idx = self.get_closest_waypoint_idx()
+				#closest_waypoint_idx = self.get_closest_waypoint_idx()
 				self.publish_waypoints()
 			rate.sleep()    
 
