b2d674e63d6fd40f8c579e119010a296ee13aa9d
michaleb <michale.bailey@gmail.com>
2019-04-05 13:51:06 -0400

    updated tl_detector

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -55,33 +55,7 @@ class TLDetector(object):
         self.state_count = 0
 
         rospy.spin()
-        #self.loop()
-
-    '''def loop(self):
-        rate = rospy.Rate(5)
-        while not rospy.is_shutdown():
-            if self.pose and self.waypoints and self.has_image:
-                light_wp, state = self.process_traffic_lights()
-
-                
-                Publish upcoming red lights at camera frequency.
-                Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
-                of times till we start using it. Otherwise the previous stable state is
-                used.
-                
-                if self.state != state:
-                    self.state_count = 0
-                    self.state = state
-                elif self.state_count >= STATE_COUNT_THRESHOLD:
-                    self.last_state = self.state
-                    light_wp = light_wp if state == TrafficLight.RED else -1
-                    self.last_wp = light_wp
-                    self.upcoming_red_light_pub.publish(Int32(light_wp)) 
-                else:
-                    self.upcoming_red_light_pub.publish(Int32(self.last_wp)) 
-                self.state_count += 1
-            rate.sleep() '''     
-
+        
     def pose_cb(self, msg):
         self.pose = msg
 
