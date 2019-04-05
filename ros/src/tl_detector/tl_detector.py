c7f0d5352a8224463bd574aa38b949e22ef82ddc
michaleb <michale.bailey@gmail.com>
2019-04-05 13:02:31 -0400

    updated tl_detector

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -210,9 +210,9 @@ class TLDetector(object):
                     closest_light = light
                     line_wp_idx = temp_wp_idx
         
-        sld = math.hypot((line[0] - self.pose.pose.position.x), (line[1] - self.pose.pose.position.y))
+        #sld = math.hypot((line[0] - self.pose.pose.position.x), (line[1] - self.pose.pose.position.y))
                
-        if closest_light and sld <= 100:
+        if closest_light and diff <= 100:
             state = self.get_light_state(closest_light)
             return line_wp_idx, state
         
